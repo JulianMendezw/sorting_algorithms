2f540cc74dabb3358d228955ade4538a961dcf50
Julian <julianlx@hotmail.com>
2021-02-01 15:48:59 -0500

    insertion sort list

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * insertion_sort_list - function that sorts an array of integers in ascending
+ *						order.
+ * @list: List to be sorted.
+ *
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *previous = NULL;
+	listint_t *current = *list;
+
+	while (current)
+	{
+		previous = current->prev;
+		while (previous)
+		{
+			if (previous->n > current->n)
+			{
+				if (previous->prev)
+					previous->prev->next = current;
+
+				if (current->next)
+					current->next->prev = previous;
+
+				previous->next = current->next;
+				current->prev = previous->prev;
+				previous->prev = current;
+				current->next = previous;
+
+				if (!current->prev)
+					*list = current,
+					current = *list;
+
+				print_list(*list);
+			}
+			previous = previous->prev;
+		}
+		current = current->next;
+	}
+}
