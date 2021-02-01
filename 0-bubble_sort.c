98def2986d9dff37ac75e831b42858352a041d09
Julian <julianlx@hotmail.com>
2021-01-31 19:51:02 -0500

    chanching ++i > i++ line 21

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -18,7 +18,7 @@ void bubble_sort(int *array, size_t size)
 
 	for (j = 0; j < size - 1; ++j)
 	{
-		for (i = 0; i < (size - j - 1); ++i)
+		for (i = 0; i < (size - j - 1); i++)
 		{
 			if (array[i] > array[i + 1])
 			{
