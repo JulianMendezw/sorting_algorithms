f1847aee25b226ba051fac39cafaad04a2249335
Julian <julianlx@hotmail.com>
2021-02-01 19:40:22 -0500

    adding statemens at line 17

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -14,7 +14,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *previous = NULL;
 	listint_t *current = *list;
 
-	if (list == NULL || *list == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	while (current)
